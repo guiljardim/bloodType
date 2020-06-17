31456cb57c0f012c0edbbd40ed472a46edc7e4f8
guilherme Jardim <guiljlima@gmail.com>
2020-06-17 19:47:06 -0300

    mudando teste

--- src/exercicio1.kt
+++ src/exercicio1.kt
@@ -1,10 +1,17 @@
 import Compatibilidade.verificarCompatibilidadeSanguinea
 
 fun main(){
-    verificarCompatibilidadeSanguinea(Doador(
-        "Guilherme", TipoSanguineo.AB, FatorRh.POSITIVO),
-        Paciente("Lucas", TipoSanguineo.O, FatorRh.POSITIVO))
-    }
+    verificarCompatibilidadeSanguinea(
+            Doador("Thais", TipoSanguineo.O, FatorRh.NEGATIVO),
+        Paciente("Joãozinho", TipoSanguineo.AB, FatorRh.POSITIVO))
+
+//    verificarCompatibilidadeSanguinea(
+//            Doador("Joãozinho", TipoSanguineo.AB, FatorRh.POSITIVO),
+//            Paciente("Thais", TipoSanguineo.O, FatorRh.NEGATIVO))
+
+}
+
+
 
 
 
